94b71d04bbf8dac5bdc76a7013b54258784a6d33
Phoenix Baker <phoenixbvu@gmail.com>
2025-10-09 13:40:54 +1100

    updated other perms files for project

--- tests/test-apps/nextjs/omnibase/permissions/tenants.ts
+++ tests/test-apps/nextjs/omnibase/permissions/tenants.ts
@@ -7,33 +7,28 @@ class Tenant implements Namespace {
     owners: User[];
     admins: User[];
     members: User[];
-    can_invite: User[];
-    can_delete: User[];
-    can_manage_billing: User[];
+    can_invite_users: User[];
+    can_update_user_role: User[];
+    can_remove_user: User[];
   };
 
   permits = {
-    invite: (ctx: Context): boolean =>
+    invite_user: (ctx: Context): boolean =>
       this.related.owners.includes(ctx.subject) ||
       this.related.admins.includes(ctx.subject) ||
-      this.related.can_invite.includes(ctx.subject),
+      this.related.can_invite_users.includes(ctx.subject),
 
-    delete: (ctx: Context): boolean =>
-      this.related.owners.includes(ctx.subject) ||
-      this.related.can_delete.includes(ctx.subject),
+    delete_tenant: (ctx: Context): boolean =>
+      this.related.owners.includes(ctx.subject),
 
-    view: (ctx: Context): boolean =>
-      this.related.owners.includes(ctx.subject) ||
-      this.related.admins.includes(ctx.subject) ||
-      this.related.members.includes(ctx.subject),
-
-    manage_members: (ctx: Context): boolean =>
-      this.related.owners.includes(ctx.subject) ||
-      this.related.admins.includes(ctx.subject),
-
-    manage_billing: (ctx: Context): boolean =>
+    remove_user: (ctx: Context): boolean =>
       this.related.owners.includes(ctx.subject) ||
       this.related.admins.includes(ctx.subject) ||
-      this.related.can_manage_billing.includes(ctx.subject),
+      this.related.can_remove_user.includes(ctx.subject),
+
+    update_user_role: (ctx: Context): boolean =>
+      this.related.owners.includes(ctx.subject) ||
+      this.related.admins.includes(ctx.subject) ||
+      this.related.can_update_user_role.includes(ctx.subject),
   };
 }
